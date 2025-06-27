bc80a2f44a3e682d41fec7012e61b298dd4a8641
BlindAxolotl <167556385+BlindAxolotl@users.noreply.github.com>
2025-06-26 20:34:02 -0500

    Added base files and first row of grid using js

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+let count = 1;
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    const cDiv = document.createElement("div");
+    cDiv.classList.add("child");
+    cDiv.textContent = `This is div ${count}`;
+    container.appendChild(cDiv);
+    count++;
+    console.log("Div Created");
+  }
+}
+
+createGrid();
